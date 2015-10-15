4b9488cefbc5eee5615f4bf86449cf1c3ee5fd48
Markus Broecker <broecker@wisc.edu>
2015-10-15 12:49:21 -0500

    Moved layout and viewport into separate file

---
+++ Layout.cpp
@@ -0,0 +1,153 @@
+#include "Layout.h"
+#include "OrbitCamera.h"
+#include "AABB.h"
+
+#include <GL/glew.h>
+
+
+using namespace std;
+using namespace glm;
+
+
+extern AABB globalBBox;
+
+void Viewport::setup() const
+{
+	glViewport(position.x, position.y, size.x, size.y);
+
+	// reset any transform
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, size.x, 0, size.y, 0, 1);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// draw a border
+	if (highlighted)
+	{
+		glColor3f(color.r, color.g, color.b);
+		glBegin(GL_LINE_LOOP);
+		glVertex2i(1, 1);
+		glVertex2i(size.x, 1);
+		glVertex2i(size.x, size.y);
+		glVertex2i(1, size.y);
+		glEnd();
+	}
+
+	// set correct matrices
+	assert(camera);
+	camera->setup();
+	
+}
+
+SingleViewLayout::SingleViewLayout(const ivec2& resolution)
+{
+	viewport.position = ivec2(0);
+	viewport.size = resolution;
+}
+
+SingleViewLayout::~SingleViewLayout()
+{
+	delete viewport.camera;
+}
+
+Viewport* SingleViewLayout::getActiveViewport()
+{
+	if (viewport.highlighted)
+		return &viewport;
+	else
+		return nullptr;
+}
+
+void SingleViewLayout::updateMouseMove(const glm::ivec2& coords)
+{
+	if (viewport.isInside(coords))
+		viewport.highlighted = true;
+	else
+		viewport.highlighted = false;
+}
+
+void SingleViewLayout::resize(const ivec2& size)
+{
+	viewport.position = ivec2(0);
+	viewport.size = size;
+	viewport.camera->aspect = (float)size.x / size.y;
+}
+
+TopViewFullLayout::TopViewFullLayout(const ivec2& resolution) : SingleViewLayout(resolution)
+{
+	viewport.name = Viewport::ORTHO_Y;
+	viewport.color = vec3(1.f);
+	viewport.camera = new OrthoCamera(vec3(0.f, -1, 0), vec3(1.f, 0.f, 0.f));
+	viewport.camera->aspect = (float)resolution.x / resolution.y;
+}
+
+
+PerspectiveFullLayout::PerspectiveFullLayout(const ivec2& resolution) : SingleViewLayout(resolution)
+{
+	viewport.camera = new OrbitCamera;
+	viewport.color = vec3(1.f);
+	viewport.name = Viewport::PERSPECTIVE;
+
+	OrbitCamera* cam = dynamic_cast<OrbitCamera*>(viewport.camera);
+	cam->far = globalBBox.getSpanLength() * 4.f;
+	cam->near = cam->near / 100.0;
+	cam->aspect = (float)resolution.x / resolution.y;
+
+	cam->radius = (globalBBox.getSpanLength() * 1.5);
+	cam->target = globalBBox.getCentroid();
+
+}
+
+FourViewLayout::FourViewLayout()
+{
+	views[0].name = Viewport::ORTHO_X;
+	views[0].camera = new OrthoCamera(glm::vec3(-1, 0, 0), glm::vec3(0.f, 1.f, 0.f));
+	views[0].color = vec3(1, 0, 0);
+
+	views[1].name = Viewport::ORTHO_Y;
+	views[1].camera = new OrthoCamera(glm::vec3(0.f, -1, 0), glm::vec3(1.f, 0.f, 0.f));
+	views[1].color = vec3(0, 1, 0);
+
+	views[2].name = Viewport::ORTHO_Z;
+	views[2].camera = new OrthoCamera(glm::vec3(0, 0.f, -1), glm::vec3(0.f, 1.f, 0.f));
+	views[2].color = vec3(0, 0, 1);
+
+	views[3].name = Viewport::PERSPECTIVE;
+	views[3].camera = new OrbitCamera;
+	views[3].color = vec3(1);
+
+}
+
+void FourViewLayout::resize(const ivec2& size)
+{
+	using namespace glm;
+
+	const float ASPECT = (float)size.x / size.y;
+	const ivec2 VIEWPORT_SIZE = size / 2;
+
+	// setup the 4 views
+	views[0].position = ivec2(0, 0);
+	views[1].position = ivec2(size.x / 2, 0);
+	views[2].position = ivec2(0, size.y / 2);
+	views[3].position = ivec2(size.x / 2, size.y / 2);
+
+
+
+	for (int i = 0; i < 4; ++i)
+	{
+		views[i].size = VIEWPORT_SIZE;
+		views[i].camera->aspect = ASPECT;
+	}
+}
+
+void FourViewLayout::updateMouseMove(const ivec2& m)
+{
+	for (int i = 0; i < 4; ++i)
+		if (views[i].isInside(m))
+			views[i].highlighted = true;
+		else
+			views[i].highlighted = false;
+
+}
---
+++ Layout.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class ICamera;
+
+struct Viewport
+{
+	// window coordinates
+	glm::ivec2		position, size;
+
+	glm::vec3		color;
+
+	enum ViewportName { ORTHO_X = 0, ORTHO_Y, ORTHO_Z, PERSPECTIVE = 3 } name;
+
+	ICamera*		camera;
+	bool			highlighted;
+
+	inline bool isInside(const glm::ivec2& cursor) const
+	{
+		using namespace glm;
+		const ivec2 rc = cursor - position;
+		return all(greaterThanEqual(rc, ivec2(0))) && all(lessThanEqual(rc, size));
+	}
+
+	inline float getAspect() const
+	{
+		return (float)size.x / size.y;
+	}
+
+
+	void setup() const;
+};
+
+
+class Layout
+{
+public:
+	virtual ~Layout() {}
+
+	virtual Viewport* getActiveViewport() = 0;
+
+	virtual void updateMouseMove(const glm::ivec2& coords) = 0;
+	virtual void resize(const glm::ivec2& size) = 0;
+
+	virtual ICamera* getPerspectiveCamera() = 0;
+
+	virtual size_t getViewCount() const = 0;
+	virtual Viewport* getView(unsigned int n) = 0;
+};
+
+
+class FourViewLayout : public Layout
+{
+public:
+	FourViewLayout();
+
+	virtual void resize(const glm::ivec2& size);
+	virtual void updateMouseMove(const glm::ivec2& m);
+
+	virtual ICamera* getPerspectiveCamera() { return views[3].camera; }
+
+private:
+	Viewport		views[4];
+};
+
+class SingleViewLayout : public Layout
+{
+public:
+	SingleViewLayout(const glm::ivec2& resolution);
+	virtual ~SingleViewLayout();
+
+	virtual Viewport* getActiveViewport();
+	virtual void updateMouseMove(const glm::ivec2& coords);
+
+	virtual void resize(const glm::ivec2& size);
+
+	inline size_t getViewCount() const { return 1; }
+	inline Viewport* getView(unsigned int n) { return &viewport; }
+
+protected:
+	Viewport			viewport;
+};
+
+
+class TopViewFullLayout : public SingleViewLayout
+{
+public:
+	TopViewFullLayout(const glm::ivec2& resolution);
+
+	inline ICamera* getPerspectiveCamera() { return nullptr; }
+
+};
+
+class PerspectiveFullLayout : public SingleViewLayout
+{
+public:
+	PerspectiveFullLayout(const glm::ivec2& resolution);
+
+	inline ICamera* getPerspectiveCamera() { return viewport.camera; }
+};
+
+
