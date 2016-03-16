b9b129fa1e5d91617634959d799a9020da73d7e0
Markus Broecker <broecker@wisc.edu>
2016-03-16 17:06:11 -0500

    Fixed some smaller bugs

--- EvaluateCalibration/main.cpp
+++ EvaluateCalibration/main.cpp
@@ -33,19 +33,16 @@ static mat4 loadRegistration(const string& filename)
 	mat4 T(1.f);
 
 
-	for (int i = 0; i < 16; ++i)
-	{
-		string buffer;
-		getline(file, buffer);
-				
-#ifdef _WIN32
-		int result = sscanf_s(buffer.c_str(), "m%*2s: %f", &glm::value_ptr(T)[i]);
-#else
-		int result = sscanf(buffer.c_str(), "m%*2s: %f", &glm::value_ptr(T)[i]);
-#endif
-		
-	}
+	float* m = value_ptr(T);
 
+	for (int i = 0; i < 16; ++i)
+		file >> m[i];
+
+
+	/*
+	cout << "Read matrix: \"" << filename << "\"\n";
+	cout << T << endl;
+	*/
 	return T;
 }
 
@@ -69,46 +66,57 @@ int main(int argc, const char** argv)
 		return 1;
 	}
 
+
+	// parse command line here
+	vector<pair<string, string>> filepairs;
 	for (int i = 1; i < argc; i += 2)
+	{
+		filepairs.push_back(make_pair(argv[i], argv[i + 1]));
+	}
+
+
+
+
+
+	for (auto it = filepairs.begin(); it != filepairs.end(); ++it)
 	{	
 		SpimStack* ref = nullptr;
 
 		try
 		{
-			string stackFile(argv[i+0]);
-
+			const string stackFile(it->first);
+			const string solutionFile(it->second);
 
 			// load references (phantoms)
 			ref = new SpimStackU16;
 			ref->load(stackFile);
 
 			// load their transforms
-			string transformFile = stackFile + ".registration.txt";
-			ref->loadRegistration(transformFile);
+			const string transformFile = stackFile + ".registration.txt";
+			ref->setTransform(loadRegistration(transformFile));
 
 
 			cout << "Reference:  " << transformFile << endl;
 			cout << "Dimensions: " << ref->getVoxelDimensions() * vec3(ref->getResolution()) << endl;
 
 			// load solutions
-			string solutionFile(argv[i + 1]);
-			mat4 solutionTransform = loadRegistration(solutionFile);
-			
-			cout << "Solution: " << solutionFile << endl;
-
+			const mat4 solutionTransform = loadRegistration(solutionFile);
+		
 			// compare 
-			vector<vec3> bboxVertices = ref->getBBox().getVertices();
-
-
+			const vector<vec3> bboxVertices = ref->getBBox().getVertices();
+			
 
 			// transform
 			double aggregatedDistance = 0;
 			for (int i = 0; i < 8; ++i)
 			{
+
 				vec4 reference = ref->getTransform() * vec4(bboxVertices[i], 1.f);
 				vec4 solution = solutionTransform * vec4(bboxVertices[i], 1.f);
-
+				
 				double d = length(reference - solution);
+
+				//cout << bboxVertices[i] << " -> " << reference << " - " << solution << " = " << d << endl;
 
 				cout << "Vertex[" << i << "] distance: " << d << endl;
 				aggregatedDistance += d;
@@ -133,13 +141,7 @@ int main(int argc, const char** argv)
 		delete ref;
 	}
 
-
-	// calculate difference based on bounding boxes here ...
-
 	
-
-
-
 #ifdef _WIN32
 	system("pause");
 #endif
